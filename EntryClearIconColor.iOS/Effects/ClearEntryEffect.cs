b24a87e6747a644247a323fe2cff3c2ba6fecb15
Depechie <glenn.versweyveld@gmail.com>
2021-03-27 23:08:51 +0100

    Initial commit

---
+++ EntryClearIconColor.iOS/Effects/ClearEntryEffect.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("Effects")]
+[assembly: ExportEffect(typeof(EntryClearIconColor.iOS.Effects.ClearEntryEffect), "ClearEntryEffect")]
+namespace EntryClearIconColor.iOS.Effects
+{
+    public class ClearEntryEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            ConfigureControl();
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        private void ConfigureControl()
+        {
+            if (Control is UITextField textField)
+            {
+                //Enable the clear icon
+                textField.ClearButtonMode = UITextFieldViewMode.WhileEditing;
+                //Get hold of the clear icon inside the text field
+                UIButton clearButton = textField.ValueForKey(new Foundation.NSString("clearButton")) as UIButton;
+                //Set the desired color
+                clearButton.TintColor = UIColor.Red;
+                UIImage clearImage = UIImage.GetSystemImage("xmark.circle.fill");
+                //Override the regular image with the colored one
+                clearButton.SetImage(clearImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate), UIControlState.Normal);
+            }
+        }
+    }
+}
---
+++ EntryClearIconColor/Effects/ClearEntryEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace EntryClearIconColor.Effects
+{
+    public class ClearEntryEffect : RoutingEffect
+    {
+        public ClearEntryEffect() : base("Effects.ClearEntryEffect")
+        {
+        }
+    }
+}
